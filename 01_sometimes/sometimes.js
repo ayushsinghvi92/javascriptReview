05c4f627b64fd5ce9884202778ba1aef85eb6c02
mosane <ayush.singhvi@nyu.edu>
2016-06-05 18:06:52 -0400

    Sommetimes complete

--- 01_sometimes/sometimes.js
+++ 01_sometimes/sometimes.js
@@ -1,1 +1,24 @@
+var sometimes = function (func) {
+	var counter = 1;
+	var retFunc = function () {
+		if (!run(counter)){
+			counter++;
+			return "I do not know!";
+		}
+		else {
+			counter++;
+			return func.apply(this, (arguments));
+		}
 
+	}
+	return retFunc;
+}
+
+var run = function (num) {
+	if(num<3)
+		return true;
+	else if (num%2)
+		return true
+	else 
+		return false;
+}

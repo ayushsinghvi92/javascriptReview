792c507fe352c7882239f221205e10f1a9a2d99b
mosane <ayush.singhvi@nyu.edu>
2016-06-05 23:07:14 -0400

    completed event emitter excercises

--- 03_event_emitter/event_emitter.js
+++ 03_event_emitter/event_emitter.js
@@ -1,1 +1,19 @@
+var MyEventEmitter = function () {
+	this.events = {};
 
+
+}
+
+MyEventEmitter.prototype.addListener = function(first_argument, second_argument) {
+	if(this.events[first_argument] == undefined) {
+		this.events[first_argument] = [];
+	}
+		this.events[first_argument].push(second_argument);
+};
+
+MyEventEmitter.prototype.emit = function(first_argument, second_argument) {
+	var arr = this.events[first_argument];
+	arr.map(function(o){
+		o.apply(this, [second_argument]);
+	});
+};
